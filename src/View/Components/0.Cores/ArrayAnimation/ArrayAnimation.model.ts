cd027545e7c7e9417610834fa495d108ede09f99
Artem Makeev <Kochebnic@gmail.com>
2025-02-02 07:03:24 +0300

    Полотно

--- src/View/Components/0.Cores/ArrayAnimation/ArrayAnimation.model.ts
+++ src/View/Components/0.Cores/ArrayAnimation/ArrayAnimation.model.ts
@@ -1,29 +1,23 @@
 import { IComponent } from "./index";
-import { ComponentProps, useEffect, useState } from "react";
+import { ComponentProps } from "react";
 import { motion } from "framer-motion";
 
 function ArrayAnimationModel(props: IComponent) {
 	const { children, type, extStyle, uniqueKey, mode } = props;
-
-	const [isFirstRender, setIsFirstRender] = useState(true);
-
-	useEffect(() => {
-		setIsFirstRender(false);
-	}, []);
 
 	const stylesType = (function (): ComponentProps<typeof motion.div> {
 		switch (type) {
 			case "sliceA":
 				return {
 					transition: { duration: 0.25 },
-					initial: isFirstRender ? false : { opacity: 0, x: -50 },
+					initial: { opacity: 0, x: -50 },
 					animate: { opacity: 1, x: 0 },
 					exit: { opacity: 0, y: -100 },
 				};
 			default:
 				return {
 					transition: { duration: 0.25 },
-					initial: isFirstRender ? false : { opacity: 0 },
+					initial: { opacity: 0 },
 					animate: { opacity: 1 },
 					exit: { opacity: 0 },
 				};
